d8cefee392438798b30554aa7e7cf7a7e3ca1912
Kutaibay <kutaiba.yhy@icloud.com>
2025-03-23 06:59:08 +0100

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Fahry.de",
+  description: "Fahrschul-Buchungstool",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="de">
+      <body>{children}</body>
+    </html>
+  );
+}
