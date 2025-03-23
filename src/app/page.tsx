04fb872faed24d71a228de9b3a7167c5534166f0
Kutaibay <kutaiba.yhy@icloud.com>
2025-03-23 06:53:12 +0100

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "4rem" }}>
+      <h1>🚗 Willkommen bei Fahry.de!</h1>
+      <p>Dein digitales Buchungstool für Fahrstunden</p>
+    </div>
+  );
+}
